52b954037012ec61b5cf531c638ab92455677a1e
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-06 23:53:23 +0530

    initial commit for D9

---
+++ Day-9/palindromeNumber.java
@@ -0,0 +1,21 @@
+public class palindromeNumber {
+    
+}
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         String x1 = Integer.toString(x);
+//         int n = x1.length();
+//         if(x1.length()<2){
+//             return true;
+//         }
+//         for(int i = 0;i<n/2;i++){
+//             if(x1.charAt(i) == x1.charAt(n-1-i)){
+//                 continue;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
