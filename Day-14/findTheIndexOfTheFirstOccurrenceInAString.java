dd3f56da309402e593739cc637052661506d4be8
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-17 23:56:56 +0530

    Initial commit for D14

---
+++ Day-14/findTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,11 @@
+public class findTheIndexOfTheFirstOccurrenceInAString {
+    
+}
+// class Solution {
+//     public int strStr(String s1, String s2) {
+//         int n1 =  s1.length();
+//         int n2 = s2.length();
+//         if(n1<n2) return -1;
+//         else return s1.indexOf(s2);
+//     }
+// }
